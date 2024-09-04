defa12d55689ed8a21c0333132d8e50beb32d5f4
Katherine Robbins <kak2819@email.vccs.edu>
2024-09-04 19:27:22 -0400

    wrote code from class

--- CloneCW/CloneCW/Program.cs
+++ CloneCW/CloneCW/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //this is a single line comment
+
+            /*multi line comment
+             * etc 
+             * etc
+             */
+
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + userName);
         }
     }
 }
