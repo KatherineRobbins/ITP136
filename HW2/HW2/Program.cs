e4c7aaf53827ecdfd15e8356a5d16806bb7866d7
Katherine Robbins <kak2819@email.vccs.edu>
2024-09-04 19:44:30 -0400

    steps 1-4 of hw

---
+++ HW2/HW2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("who is being seen today");
+            string patientNname = Console.ReadLine();
+            Console.WriteLine("welcome: " +  patientNname);
+        }
+    }
+}
