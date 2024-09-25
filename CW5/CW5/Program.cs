79974d7d8b8d6bf41ed6f46bd782ba47913cef55
Katherine Robbins <kak2819@email.vccs.edu>
2024-09-25 18:34:02 -0400

    while loop and for loop

---
+++ CW5/CW5/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+namespace CW5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+
+            int a, b;
+
+            //a = 0;
+            //b = 5;
+
+            // while(a < b)
+            // {
+            //   WriteLine(a);
+
+            //   a++;
+            // }
+
+            //for loop
+
+
+            //variables
+            int total;
+
+            WriteLine("Welcome to my calculator");
+
+
+            for(int k = 0; k < 3; k++)
+            {
+                WriteLine("Enter your first number");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                WriteLine("Enter your second number");
+                b =  Convert.ToInt32(Console.ReadLine());
+
+                total = a + b;
+                WriteLine("Your total is: " + total);
+            }
+            WriteLine("Thank you for using my calculator");
+            ReadLine();
+        }
+    }
+}
