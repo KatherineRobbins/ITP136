058b6da7984779f63b456c3d9711c04cdd305973
Katherine Robbins <kak2819@email.vccs.edu>
2024-09-30 12:39:02 -0400

    loops with while

---
+++ HW5/HW5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+using static System.Console;
+
+namespace HW5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            a = 0;
+            b = 10;
+            
+
+            Console.WriteLine("Count by 1's up to 10!");
+
+            while ( a <= b)
+            {
+                WriteLine(a);
+                    a++;
+            }
+            
+            
+
+            
+        }
+    }
+}
