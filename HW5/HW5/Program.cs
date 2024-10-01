dcbe34a8eca1299be77a2ba20d82c22e617dd82a
Katherine Robbins <kak2819@email.vccs.edu>
2024-10-01 19:59:28 -0400

    added user input

--- HW5/HW5/Program.cs
+++ HW5/HW5/Program.cs
@@ -9,17 +9,25 @@ namespace HW5
         {
             int a, b;
 
-            a = 0;
+            
             b = 10;
             
 
-            Console.WriteLine("Count by 1's up to 10!");
+            Console.WriteLine("WHILE LOOP: Count by 1's up to 10!");
+            Console.WriteLine("Enter a number between 1-10");
+            a = Convert.ToInt32(Console.ReadLine());
 
             while ( a <= b)
             {
+                
+                
                 WriteLine(a);
                     a++;
+
             }
+
+            WriteLine("Your can now count by 1's using a WHILE LOOP");
+            ReadLine();
             
             
 
