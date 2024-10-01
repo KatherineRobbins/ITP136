c3a5c0fbb6675b44478838744812253af098eecb
Katherine Robbins <kak2819@email.vccs.edu>
2024-10-01 19:53:46 -0400

    FOR LOOP

---
+++ HW5-2/HW5-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Schema;
+using static System.Console;
+
+namespace HW5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a;
+           
+            
+            Console.WriteLine("FOR LOOP:Count by 1's up to 10!");
+
+            WriteLine("Enter a number between 1-10");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = a; x<=10; x++)
+            {
+
+               
+                WriteLine(x);
+                
+
+               
+            }
+            WriteLine("You can now count by 1's using a FOR LOOP");
+            ReadLine();
+        }
+    }
+}
