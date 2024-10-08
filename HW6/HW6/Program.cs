815608cd6711ca9c0df4522f572a455a518b38cd
Katherine Robbins <kak2819@email.vccs.edu>
2024-10-08 12:27:39 -0400

    START

---
+++ HW6/HW6/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace HW6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            welcomeMessage();
+        }
+
+        static void welcomeMessage()
+        {
+            WriteLine("Welcome to my week 6 assignment where we work on methods!");
+
+            ReadKey();
+        }
+
+        static int magicNumber()
+        {
+            int m;
+            WriteLine("Please enter your magic number!");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            return m;
+        }
+    }
+}
