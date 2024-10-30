83aff96ac74712589c913d7d3f38b22f79de9025
Katherine Robbins <kak2819@email.vccs.edu>
2024-10-30 18:48:34 -0400

    cw10

---
+++ CW10/CW10/Program.cs
@@ -0,0 +1,52 @@
+﻿using static System.Console;
+
+namespace CW10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //intro to objects
+            // create objects from classes
+
+
+            employee emp1 = new employee();
+            employee emp2 = new employee();
+
+
+            emp1.EmployeeId = 1234;
+            emp1.LastName = "Robbins";
+            emp1.FirstName = "Katherine";
+
+            emp2.EmployeeId = 5678;
+            emp2.LastName = "Kearney";
+            emp2.FirstName = "Sarah";
+
+            WriteLine("{0,10}{1,15}{2,15}", "Employee Id", "Last Name", "First Name");
+            WriteLine("{0,10}{1,15}{2,15}", emp1.EmployeeId, emp1.LastName, emp1.FirstName);
+            WriteLine("{0,10}{1,15}{2,15}", emp2.EmployeeId, emp2.LastName, emp2.FirstName);
+
+            emp1.LastName = "Smith";
+
+            WriteLine("{0,10}{1,15}{2,15}", emp1.EmployeeId, emp1.LastName, emp1.FirstName);
+
+
+            WriteLine("Enter Employee Id");
+            emp2.EmployeeId = Convert.ToInt32(ReadLine());
+
+            WriteLine("Enter Employee Last Name");
+            emp2.LastName = ReadLine();
+
+            WriteLine("Enter Employee First Name");
+            emp2.FirstName = ReadLine();
+
+            WriteLine("{0,10}{1,15}{2,15}", emp2.EmployeeId, emp2.LastName, emp2.FirstName);
+
+            ReadKey();
+
+
+        }
+    }
+}
---
+++ CW10/CW10/employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW10
+{
+    public class employee
+    {
+
+        //base class add properties
+        // changed from internal to public , we want program class to see employee class
+
+
+        int empId;
+        string fName, lName;
+
+        // add property Properties are like variables as they hold values for the class and can be accessed by other classes
+
+
+        public int EmployeeId
+        {
+
+            // two methods get and set
+            get { return empId; }
+            set { empId = value; }
+        }
+
+        public string LastName
+        {
+            get { return lName; }
+            set { lName = value; }
+
+
+        }
+
+        public string FirstName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+    }
+}
