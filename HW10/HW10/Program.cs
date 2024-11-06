38cd80b8e287759663dc0a9bd4cc128c7bdd2e28
Katherine Robbins <kak2819@email.vccs.edu>
2024-11-06 00:34:53 -0500

    hw10

---
+++ HW10/HW10/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+namespace HW10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            food food1 = new food();
+            food food2 = new food();
+
+
+            food1.FoodId = 1234;
+            food1.FoodName = "Pear";
+            food1.FoodDescription = "Fruit";
+            food1.FoodCost = 1.00;
+
+            food2.FoodId = 5678;
+            food2.FoodName = "Okra";
+            food2.FoodDescription = "Vegetable";
+            food2.FoodCost = 1.25;
+
+
+
+            WriteLine("{0,10}{1,17}{2,20}{3,15}", "Food Id", "Food Name", "Food Description", "Food Cost");
+            WriteLine("{0,10}{1,17}{2,20}{3,15}", food1.FoodId, food1.FoodName, food1.FoodDescription, food1.FoodCost);
+            WriteLine("{0,10}{1,17}{2,20}{3,15}", food2.FoodId, food2.FoodName, food2.FoodDescription, food2.FoodCost);
+        }
+    }
+}
---
+++ HW10/HW10/food.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10
+{
+    internal class food
+    {
+
+        int fID;
+        string fName;
+        string fdescription;
+        double fcost;
+
+        public int FoodId
+        {
+            get { return fID; }
+            set { fID = value; }
+        }
+
+        public string FoodName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        public string FoodDescription
+        {
+            get { return fdescription; }
+            set { fdescription = value; }
+
+        }
+
+        public double FoodCost
+        {
+            get { return fcost; }
+            set { fcost = value; }
+        }
+
+    }
+}
