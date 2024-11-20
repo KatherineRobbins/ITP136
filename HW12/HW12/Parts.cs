783dbd65576327fbf63ef94059a8508ade81efb2
Katherine Robbins <kak2819@email.vccs.edu>
2024-11-20 18:04:33 -0500

    cw12

---
+++ HW12/HW12/Parts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    public class Parts
+    {
+
+        int partNum;
+        string partName, partDescription;
+        double partCost;
+
+        public Parts(int pNum, string pName, string pDescription, double pCost)
+        {
+            partNum = pNum;
+            partName = pName;
+            partDescription = pDescription;
+            partCost = pCost;
+        }
+
+        public int PartNumber
+        {
+            get { return partNum; }
+            set { partNum = value; }
+        }
+
+        public string PartName
+        {
+            get { return partName;}
+            set { partName = value; }
+        }
+
+        public string PartDescription
+        {
+            get { return partDescription;}
+            set { partDescription = value; }
+        }
+
+        public double PartCost
+        {
+            get { return partCost;}
+            set { partCost = value; }
+        }
+    }
+}
---
+++ HW12/HW12/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace HW12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
